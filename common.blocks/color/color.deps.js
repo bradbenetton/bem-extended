b8757dd5647b398f8fa729fc91422179e1906599
bradbenetton <bradbenetton@gmail.com>
2015-06-01 00:16:30 +0300

    update

---
+++ common.blocks/color/color.deps.js
@@ -0,0 +1,12 @@
+({
+    mustDeps: [],
+    shouldDeps: [{
+        block: 'color',
+        mods: {
+            bg: [
+                'dark', 'blue', 'green', 'red', 'yellow', 'turquoise'
+            ],
+            squared: true
+        }
+    }]
+});
